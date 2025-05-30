ea28dd7ebd4d783706993b7074f6f5fa9d728ed6
ashdroidcmd <z.wolff25@gmail.com>
2025-05-30 13:58:53 +0800

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
+import { Analytics } from "@vercel/analytics/react"
 import MainLayout from './layout/MainLayout'
 import ScrollToTop from './utils/ScrollToTop'
 import HomePage from './pages/HomePage'
@@ -11,6 +12,7 @@ function App() {
   return (
     <>
       <BrowserRouter>
+      <Analytics />
       <ScrollToTop />
         <Routes>
           <Route path="/" element={<MainLayout />}>
