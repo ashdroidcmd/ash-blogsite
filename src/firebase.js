a04f1cfa934d00b444e56a43aeda4b59e10e82bd
ashdroidcmd <z.wolff25@gmail.com>
2025-06-13 19:31:21 +0800

    fix firebase authentication

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -16,4 +17,5 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const db = getFirestore(app);
 
+export const auth = getAuth(app);
 export { db };
